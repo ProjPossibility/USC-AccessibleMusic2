2b435cf08127453d1e75d92cfeea112181c27d7a
SylikC <sylikc@duran.(none)>
2012-01-21 15:47:28 -0800

    asdf

--- RDio_PHP/web-based.php
+++ RDio_PHP/web-based.php
@@ -1,4 +1,6 @@
 <?php
+include_once('lib/debug.php');
+
 # (c) 2011 Rdio Inc
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
