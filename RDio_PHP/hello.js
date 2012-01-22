a34beeb34dcf212711fac0ac7fb75a627fb27425
SylikC <sylikc@duran.(none)>
2012-01-21 18:19:22 -0800

    Sat Jan 21 18:19:22 PST 2012

--- RDio_PHP/hello.js
+++ RDio_PHP/hello.js
@@ -50,7 +50,8 @@ $(document).ready(function() {
   $('#next').click(function() { apiswf.rdio_next(); });
   $('#searchbutton').click(function(){ 
 	  alert("Clicked");
-	  //var phpURL = "player.php";
+	  var phpURL = "player.php";
+	  var ajax_load = "<img src='img/load.gif' alt='loading...' />";
 	  //$('#searchsuggest').html(ajax_load).load(phpURL, "query=test");
 	  //$('#searchsuggest').load(phpURL);
   });
