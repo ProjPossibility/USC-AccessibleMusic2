ff4f4b7538676b75238c2325d79d1a1435185073
tallevy <tallevy@usc.edu>
2012-01-22 09:37:33 -0800

    fixing pound signs for search

--- website/js/search_jump.js
+++ website/js/search_jump.js
@@ -3,8 +3,8 @@ function search_play( key ) {
 	var search_field = document.getElementById('play_key');
 	//alert(search_field.value);
 	search_field.value = key;
-	var play_button = document.getElementById('play');
-	var stop_button = document.getElementById('stop');
+	var play_button = $('#play')
+	var stop_button = $('#stop');
 	//alert(play_button);
 	play_button.click();
 	stop_button.click();
