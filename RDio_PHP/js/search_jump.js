7afe7f0c281c2115ff1bdb82364e47e97b93d29a
SylikC <sylikc@duran.(none)>
2012-01-22 00:32:49 -0800

    Test Sun Jan 22 00:32:49 PST 2012

--- RDio_PHP/js/search_jump.js
+++ RDio_PHP/js/search_jump.js
@@ -1,7 +1,9 @@
 function search_play( key ) {
-	alert(document.title);
+	//alert(document.title);
 	var search_field = document.getElementById('play_key');
-	alert(search_field.value);
+	//alert(search_field.value);
+	search_field.value = key;
 	var play_button = document.getElementById('play');
-	alert(play_button);
+	//alert(play_button);
+	play_button.click();
 }
