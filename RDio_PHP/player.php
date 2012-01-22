f90a0cd3764a8e7411e0d4febd105b7143fbe884
tallevy <tallevy@usc.edu>
2012-01-21 22:48:34 -0800

    $$$

--- RDio_PHP/player.php
+++ RDio_PHP/player.php
@@ -27,16 +27,16 @@ if ($search_type != "All"){
 		$process[$i]["icon"] = $value->icon;
 		if(type != "r"){
 			if (type != "p"){
-				$process[i]["explicit"] = $value->isExplicit;
+				$process[$i]["explicit"] = $value->isExplicit;
 			} else {
-				$process[i]["artist"] = $value->owner;
-				$process[i]["artistKey"] = $value->ownerKey;
+				$process[$i]["artist"] = $value->owner;
+				$process[$i]["artistKey"] = $value->ownerKey;
 			}
-			$process[i]["length"] = $value->length;
-			$process[i]["artist"] = $value->artist;
-			$process[i]["artistKey"] = $value->artistKey;
+			$process[$i]["length"] = $value->length;
+			$process[$i]["artist"] = $value->artist;
+			$process[$i]["artistKey"] = $value->artistKey;
 		}
-		$i++;
+		($i)++;
 	}
 } else {
 	echo "search suggestions";
