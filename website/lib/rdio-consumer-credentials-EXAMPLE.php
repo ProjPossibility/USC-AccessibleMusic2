473bb6e87adfb731a87c17ed95310e4c46960b42
tallevy <tallevy@usc.edu>
2012-01-22 01:45:20 -0800

    lib and keypress

---
+++ website/lib/rdio-consumer-credentials-EXAMPLE.php
@@ -0,0 +1,9 @@
+<?php
+# you can get these by signing up for a developer account at:
+# http://developer.rdio.com/
+
+xyu4k4p2r48p3pec6z8fsupa
+define('RDIO_CONSUMER_KEY', '');
+define('RDIO_CONSUMER_SECRET', '');
+
+?>
---
+++ website/lib/rdio-credentials.php
@@ -0,0 +1,9 @@
+<?php
+# you can get these by signing up for a developer account at:
+# http://developer.rdio.com/
+
+// Kevin's key/secret xyu4k4p2r48p3pec6z8fsupa/pYvb45Xd5D
+define('RDIO_CONSUMER_KEY', 'xyu4k4p2r48p3pec6z8fsupa');
+define('RDIO_CONSUMER_SECRET', 'pYvb45Xd5D');
+
+?>
