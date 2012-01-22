a25b457e58370906e5a48cc7adfefa70ff9d24a7
tallevy <tallevy@usc.edu>
2012-01-21 18:31:30 -0800

    query

--- RDio_PHP/player.php
+++ RDio_PHP/player.php
@@ -7,14 +7,12 @@ require_once 'lib/debug.php';
 define('CONSUMER_KEY', 'xyu4k4p2r48p3pec6z8fsupa');
 define('CONSUMER_SECRET', 'pYvb45Xd5D');
 
-echo 'asdfasdfsadf';
+echo $_QUERY["query"];
 $rdio = new Rdio(array(CONSUMER_KEY, CONSUMER_SECRET));
 $searchResults = $rdio->call("searchSuggestions", array("query" => "pumped up kicks"));
-echo "<pre>";
-var_dump($searchResults);
-echo "</pre>";
-//foreach($searchResults->result as $key => $value){
-	//echo $key . "   " . $value->key . "<br>";
-//}
+
+foreach($searchResults->result as $key => $value){
+	echo $key . "   " . $value->key . "<br>";
+}
 
 ?>
