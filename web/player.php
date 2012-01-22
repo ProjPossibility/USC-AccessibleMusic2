1507f9a421de0776f052f8e8c9f1ab04083cdbb7
tallevy <tallevy@usc.edu>
2012-01-22 15:42:12 -0800

    onload

--- web/player.php
+++ web/player.php
@@ -57,6 +57,11 @@ require_once('player_auth.php');
 			'propagate':true,
 			'target':document
 		});
+
+		var phpURL = "search.php";
+		var ajax_load = "<img src='img/load.gif' alt='loading...' />";
+		$('#searchsuggest').html(ajax_load).load(phpURL, "query=" + $('#query').val() + "&type=" + "onload");
+			
 	}
 	window.onload=init();
 	function searchEnter(){
