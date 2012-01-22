c1e4c84e7348f47d1f89ca9cf8f2683de04b8cfa
tallevy <tallevy@usc.edu>
2012-01-21 22:44:52 -0800

    uncomment?

--- RDio_PHP/player.php
+++ RDio_PHP/player.php
@@ -20,7 +20,7 @@ if ($search_type != "All"){
 	$i = 0;
 	$process = array();
 	foreach($searchResults->result->results as $value){
-// 		$process[i] = array();
+		$process[i] = array();
 // 		$process[i]["name"] = $value->name;
 // 		$process[i]["type"] = $value->type;
 // 		$process[i]["key"] = $value->key;
