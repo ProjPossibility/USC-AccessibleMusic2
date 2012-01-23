52b25fbd2436de38891c47c41b29e349903193fc
SylikC <sylikc@duran.(none)>
2012-01-22 16:28:20 -0800

    Sun Jan 22 16:28:20 PST 2012

--- web/lib/debug.php
+++ web/lib/debug.php
@@ -2,7 +2,7 @@
 
 // change this to off when debug is complete (production)
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 
 ?>
